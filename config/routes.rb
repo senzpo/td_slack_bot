d963a4e4cbc75cbdd4d501b1a0e15696a7ab50d2
Evgenii Sendziuk <evgeniisendziuk@taxdome.com>
2023-06-11 17:39:09 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # get 'help', 'welcome#show'
+  # post 'help', 'welcome#show'
+
+  # root "welcome#index"
+end
