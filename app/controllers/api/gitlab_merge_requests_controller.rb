54179df483915ccbdbc3ff8ac97d92767bba1d75
Dmitrii Suchkov <dsuchkov@taxdome.com>
2023-07-05 18:31:56 +0200

    add project_id to object

--- app/controllers/api/gitlab_merge_requests_controller.rb
+++ app/controllers/api/gitlab_merge_requests_controller.rb
@@ -4,7 +4,7 @@ class Api::GitlabMergeRequestsController < ApplicationController
   def index
     project = GitlabHelper.project
     raw_merge_requests = GitlabHelper.get_merge_requests
-    merge_requests = raw_merge_requests.map { |merge_request| { id: merge_request['iid'], title: merge_request['title'], state: merge_request['state'], author: merge_request['author']['name'], reviewers: merge_request['reviewers'].map(&:name) } }
+    merge_requests = raw_merge_requests.map { |merge_request| { id: merge_request['iid'], project_id: merge_request['project_id'], title: merge_request['title'], state: merge_request['state'], author: merge_request['author']['name'], reviewers: merge_request['reviewers'].map(&:name) } }
 
     render json: {merge_requests: merge_requests.to_json}
   end
