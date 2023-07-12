3a0b2015012e1ee19a18aa519f18235c280b57fd
Dmitrii Suchkov <dsuchkov@taxdome.com>
2023-07-12 15:17:38 +0200

    add gitlab_merge_requests index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   scope module: :web do
     root 'welcome#index'
+    resources :gitlab_merge_requests, only: %i[index]
   end
 
   namespace 'api', api_scope: true, defaults: { format: :json } do
