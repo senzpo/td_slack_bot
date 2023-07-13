5f1cbd0b5167260cf691ab9f9ca43aa1c2df5227
Dmitrii Suchkov <dsuchkov@taxdome.com>
2023-07-13 10:13:28 +0200

    refactoring refresh service

--- app/services/gitlab_merge_requests/refresh.rb
+++ app/services/gitlab_merge_requests/refresh.rb
@@ -3,14 +3,15 @@
 class GitlabMergeRequests::Refresh
   def self.perform(raw_merge_requests)
     raw_merge_requests.each do |rmr|
-      params = { iid: rmr['iid'], title: rmr['title'], project_id: rmr['project_id'], state: rmr['state'] }
+      params = {
+        iid: rmr['iid'],
+        title: rmr['title'],
+        project_id: rmr['project_id'],
+        state: rmr['state']
+      }
 
-      merge_request = Gitlab::MergeRequest.find_by(iid: rmr['iid'])
-      if merge_request.blank?
-        Gitlab::MergeRequest.create!(params)
-      else
-        merge_request.update!(params)
-      end
+      merge_request = Gitlab::MergeRequest.find_or_initialize_by(iid: rmr['iid'])
+      merge_request.update!(params)
     end
   end
 end
