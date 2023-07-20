aead0de0859a02933ba77d1990a4df27ee5ab1d6
Alexander Podlesnyi <apodlesnij@taxdome.com>
2023-07-20 13:23:10 +0300

    Add event

--- app/models/bitbucket/pull_request.rb
+++ app/models/bitbucket/pull_request.rb
@@ -9,5 +9,20 @@ module Bitbucket
     validates :state, presence: true
     validates :author, presence: true
     validates :created_on, presence: true
+
+    after_create :set_event
+    after_update :check_state_change
+
+    private
+
+    def set_event
+      Event.create(entity_id: self.id, entity: 'bitbucket', status: self.state, created_at: self.created_on)
+    end
+
+    def check_state_change
+      if saved_change_to_state?
+        Event.create(entity_id: self.id, entity: 'bitbucket', status: self.state, created_at: self.updated_on)
+      end
+    end
   end
 end
