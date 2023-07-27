3235c72f4c8d588b1891f93e6f858901cf9fcb1f
Ivan Sidorin <isidorin@taxdome.com>
2023-07-27 15:58:46 +0400

    Add page links in sidebar

--- app/controllers/web/taxdome_members_controller.rb
+++ app/controllers/web/taxdome_members_controller.rb
@@ -1,6 +1,7 @@
 class Web::TaxdomeMembersController < ApplicationController
     def index
         slack_client = Slack::Web::Client.new
-        @td_members = slack_client.users_list.members
+        existing_td_members_without_bots = slack_client.users_list.members.select {|m| !m.is_bot && !m.deleted }
+        @td_members = existing_td_members_without_bots
     end
 end
