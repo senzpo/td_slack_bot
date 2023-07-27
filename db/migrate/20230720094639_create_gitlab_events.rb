3394cf90aa67720411890f466215d5c34a8a14c8
Dmitrii Suchkov <dsuchkov@taxdome.com>
2023-07-27 15:57:51 +0200

    add megrations

---
+++ db/migrate/20230720094639_create_gitlab_events.rb
@@ -0,0 +1,10 @@
+class CreateGitlabEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :gitlab_merge_request_events do |t|
+      t.references :gitlab_merge_request, foreign_key: true, null: false
+      t.string :status, null: false
+      t.datetime :produced_at, null: false
+      t.timestamps
+    end
+  end
+end
