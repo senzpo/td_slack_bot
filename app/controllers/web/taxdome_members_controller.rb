14b72622e0a7973a341979b81b866d10bea51006
Ivan Sidorin <isidorin@taxdome.com>
2023-08-13 15:53:42 +0400

    Add slack-ruby-client

--- app/controllers/web/taxdome_members_controller.rb
+++ app/controllers/web/taxdome_members_controller.rb
@@ -3,6 +3,8 @@
 module Web
   class TaxdomeMembersController < ApplicationController
     def index
+      slack_client = Slack::Web::Client.new
+      @td_members = slack_client.users_list.members
     end
   end
 end
