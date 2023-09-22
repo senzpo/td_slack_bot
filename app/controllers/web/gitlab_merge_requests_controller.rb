765a75ce81ffa637c12d1e23b69c33fceafe753e
Evgenii Sendziuk <evgeniisendziuk@taxdome.com>
2023-09-22 18:26:29 +0300

    Add paginator

--- app/controllers/web/gitlab_merge_requests_controller.rb
+++ app/controllers/web/gitlab_merge_requests_controller.rb
@@ -4,6 +4,7 @@ module Web
   class GitlabMergeRequestsController < ApplicationController
     def index
       @merge_requests = Gitlab::MergeRequest.ordered_by_newest.preload(:taxdome_member).ransack(params[:q]).result
+      @merge_requests = @merge_requests.page(params[:page])
     end
 
     def show
